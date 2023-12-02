fix gradboost gradient + reg0 + constants

Noisy-Ensemble-Regression-PyProject/NoisyRegSim_unified.py
@@ -37,7 +37,7 @@ min_sample_leaf = 1
 n_snr_pts = 10
 snr_db_vec = np.linspace(-25, 25, n_snr_pts)  # simulated SNRs [dB]
 n_repeat = 75  # Number of iterations for estimating expected performance
-sigma_profile_type = "noiseless_even"  # uniform / single_noisy / noiseless_even (for GradBoost)
+sigma_profile_type = "uniform"  # uniform / single_noisy / noiseless_even (for GradBoost)
 noisy_scale = 20
 
 n_samples = 1000  # Size of the (synthetic) dataset  in case of synthetic dataset
@@ -45,10 +45,10 @@ train_noise = 0.01  # Standard deviation of the measurement / training noise in 
 
 # data_type_vec = ["kc_house_data"]  # kc_house_data / diabetes / white-wine / sin / exp / make_reg
 data_type_vec = ["sin", "exp", "diabetes", "make_reg", "white-wine", "kc_house_data"]
-# data_type_vec = ["sin", "exp"]
+# data_type_vec = ["white-wine", "kc_house_data"]
 
 criterion = "mae"  # "mse" / "mae"
-reg_algo = "Bagging"  # "GradBoost" / "Bagging"
+reg_algo = "GradBoost"  # "GradBoost" / "Bagging"
 bagging_method = "gem"  # "bem" / "gem" / "lr"
 gradboost_robust_flag = True
 
@@ -95,8 +95,8 @@ elif reg_algo == "GradBoost":
         "exp": 1e-3,
         "make_reg": 1e-3,
         "diabetes": 1e-4,
-        "white-wine": 25e-1,
-        "kc_house_data": 1e-2
+        "white-wine": 1e-3,
+        "kc_house_data": 1e-3
     }
     gd_learn_rate_dict_r = {  # learning rate for grad-dec per dataset: MAE, GradBoost - Robust
         "sin": 1e-3,

Noisy-Ensemble-Regression-PyProject/robustRegressors/rGradBoost.py
@@ -46,9 +46,9 @@ class rGradBoost:
         # set the criterion(s)
         self.criterion = criterion
         if self.criterion == "mse":
-            self.weak_lrnr_criterion = "squared_error"
-        elif criterion == "mae":
-            self.weak_lrnr_criterion = "absolute_error"
+            self.weak_lrnr_criterion = "mse"
+        elif self.criterion == "mae":
+            self.weak_lrnr_criterion = "mse"  # "absolute_error"
 
         # Weak learner list
         self.weak_learners = []
@@ -68,8 +68,8 @@ class rGradBoost:
                 # deriv = -np.sign(G-Y).sum()
             else:
                 s0 = np.sqrt(self.TrainNoiseCov[0, 0])
-                G_Y_s0 = (G-Y) / s0
-                cost_mat = np.sqrt(2/np.pi) * s0 * np.exp(-0.5 * G_Y_s0**2) + (G-Y) * (1 - 2*sp.stats.norm.cdf(-G_Y_s0))
+                G_Y_s0 = (G - Y) / (G * s0)
+                cost_mat = np.sqrt(2/np.pi) * G * s0 * np.exp(-0.5 * G_Y_s0**2) + (G-Y) * (1 - 2*sp.stats.norm.cdf(-G_Y_s0))
                 # deriv = -np.sqrt(2/np.pi) * G_Y_s0*s0 * gaussExpTerm * (1-2*sp.stats.norm.cdf(-G_Y_s0)) + 2*G_Y_s0 * gaussExpTerm/np.sqrt(2*np.pi*s0**2)
             # g_idx = np.argmin(np.abs(deriv.sum(axis=0)))
             cost = cost_mat.mean(axis=0)
@@ -90,7 +90,7 @@ class rGradBoost:
 
         n_samples = len(y)
         if criterion == "mse":
-            self.reg0 = np.mean(y)  # mean target value over dataset
+            self.reg0 = np.mean(y) / (1 + self.TrainNoiseCov[0, 0])  # mean target value over dataset
         elif criterion == "mae":
             if self.RobustFlag:
                 self.reg0 = reg0_line_search(self)
@@ -102,7 +102,11 @@ class rGradBoost:
         self._predictions = self._predictions_wl
 
         # Initialize residuals
-        self._residuals = y - self._predictions
+        self.criterion = criterion
+        if self.criterion == "mse":
+            self._residuals = y - self._predictions
+        elif criterion == "mae":
+            self._residuals = np.sign(y - self._predictions)
 
     ''' - - - LAE GradBoost: Unconstrained weight optimization - - - '''
     def fit(self, X, y, m: int = 10):